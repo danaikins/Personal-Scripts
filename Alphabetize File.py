72aa88b2dbc0f798f42a0e64db7e91f9d783f55b
daikins <dan.aikins@gmail.com>
2018-08-30 15:44:31 -0400

    logic works

--- Alphabetize File.py
+++ Alphabetize File.py
@@ -2,3 +2,11 @@
 # Each row of the file should have one string
 # Will read all of the lines in the file, then print all of them in alphabetic order
 
+with open('../files/variables_i_need.txt', 'r') as open_file:
+    all_text = open_file.read()
+
+each_line = all_text.split()
+each_line.sort()
+
+for line in each_line:
+    print(line)
